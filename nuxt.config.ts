29ac006f4a2b028c25a28c0f6eb6286d1aa33ab9
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-10-01 23:29:16 +0900

    chore: Add @nuxt/test-utils/module to nuxt.config.ts and update devDependencies in package.json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ['@nuxt/test-utils/module'],
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true }
 })
