d671f62e957386a9ac7b645e5f6dd86737667038
Motoaki Tanaka <katachiwotodomete@gmail.com>
2024-10-01 23:29:10 +0900

    refactor: Refactor app structure to use TodoApp component

--- app.vue
+++ app.vue
@@ -1,26 +1,7 @@
 <template>
-  <div>
-    <h1>TODOリスト</h1>
-    <TodoForm @add-todo="addTodo" />
-    <TodoList :todos="todos" @toggle-todo="toggleTodo" />
-  </div>
+  <TodoApp />
 </template>
 
 <script setup>
-import { ref } from 'vue'
-import TodoList from './features/todo/TodoList.vue'
-import TodoForm from './features/todo/TodoForm.vue'
-
-const todos = ref([])
-
-const addTodo = (title) => {
-  todos.value.push({ id: Date.now(), title, completed: false })
-}
-
-const toggleTodo = (id) => {
-  const todo = todos.value.find(todo => todo.id === id)
-  if (todo) {
-    todo.completed = !todo.completed
-  }
-}
+import TodoApp from './features/todo/TodoApp.vue'
 </script>
---
+++ features/todo/TodoApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>TODOリスト</h1>
+    <TodoForm @add-todo="addTodo" />
+    <TodoList :todos="todos" @toggle-todo="toggleTodo" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import TodoList from './TodoList.vue'
+import TodoForm from './TodoForm.vue'
+
+const todos = ref([])
+
+const addTodo = (title) => {
+  todos.value.push({ id: Date.now(), title, completed: false })
+}
+
+const toggleTodo = (id) => {
+  const todo = todos.value.find(todo => todo.id === id)
+  if (todo) {
+    todo.completed = !todo.completed
+  }
+}
+</script>
